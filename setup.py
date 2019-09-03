fbbdbd673f9c0f4d88e576bc539146f97e18a6bb
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-09-02 22:10:34 -0400

    added dependency on bcrypt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='topkek',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -11,6 +11,7 @@ setup(
         'Flask-Security',
         'Flask-SQLAlchemy',
         'SQLAlchemy',
-        'PyMySQL'
+        'PyMySQL',
+        'bcrypt'
     ],
 )
