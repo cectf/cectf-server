5d4692249fc2eb24db5ab2ec7d5db755bd65f6a6
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-09-03 19:37:22 -0400

    added descriptions to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='topkek-server',
     version='1.0.7',
+    description='Backend for the topkek CTF',
+    long_description=long_description,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
