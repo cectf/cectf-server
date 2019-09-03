7c61a385fa11f616382ae785c1a09c6d1254f440
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-09-02 21:59:21 -0400

    forgot a comma

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='topkek',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'Flask',
         'Flask-Security',
-        'Flask-SQLAlchemy'
+        'Flask-SQLAlchemy',
         'SQLAlchemy',
         'PyMySQL'
     ],
