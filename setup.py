0f96b40bab450579c4e8afa849a6e8922270b9cc
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-09-03 19:37:44 -0400

    finsihed adding descriptions to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ with open("README.md", "r") as fh:
 setup(
     name='topkek-server',
     version='1.0.7',
+    author="Daniel Chiquito",
+    author_email="daniel.chiquito@gmail.com",
     description='Backend for the topkek CTF',
     long_description=long_description,
+    url="https://github.com/dchiquit/topkek-server",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
