ca3b90773a078fe47c83e24ab283f2d1b43137b9
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-09-02 21:54:12 -0400

    adding depency on Flask-SQLAlchemy

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='topkek',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
-        'flask-security',
+        'Flask',
+        'Flask-Security',
+        'Flask-SQLAlchemy'
         'SQLAlchemy',
         'PyMySQL'
     ],
