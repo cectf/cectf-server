9f85f3939c2f65190c495fb87b3ca9928de9463c
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-10-10 21:25:45 -0400

    updated setup_workspace.sh

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -9,5 +9,5 @@ pip install -r test_requirements.txt
 mkdir instance
 echo "SECRET_KEY='dev'" >> instance/config.py
 echo "SECURITY_PASSWORD_SALT='salty'" >> instance/config.py
-echo "SQLALCHEMY_DATABASE_URI='mysql+pymysql://localhost/dev'" >> instance/config.py
+echo "SQLALCHEMY_DATABASE_URI='mysql+pymysql://travis@localhost/dev'" >> instance/config.py
 echo "CECTF_FRONTEND_DOMAIN = 'http://localhost'" >> instance/config.py
