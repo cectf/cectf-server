f57455d5b470eb3be1a2427d9a573f0393154291
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-10-28 23:46:31 -0400

    advancing to 1.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cectf-server',
-    version='1.0.12',
+    version='1.0.13',
     author="Daniel Chiquito",
     author_email="daniel.chiquito@gmail.com",
     description='Backend for the CECTF',
